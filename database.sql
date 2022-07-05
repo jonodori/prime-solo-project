6a23303cdc39674553ff37edfddb588941b4d7b6
Jonathan Palalay <ring5pun@hotmail.com>
2022-07-05 16:27:39 -0500

    created database.sql

--- database.sql
+++ database.sql
@@ -1,10 +1,60 @@
-
+-- Create Database 'local_e-sports'
 -- USER is a reserved keyword with Postgres
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "username" VARCHAR (255) UNIQUE NOT NULL,
+    "password" VARCHAR (32) NOT NULL,
+    "city" text,
+    "zip_code" VARCHAR (5), 
+    "organizer" boolean
 );
+
+
+CREATE TABLE "tournaments" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (50) NOT NULL,
+    "start_date" date,
+    "end_date" date,
+    "city" text,
+    "zip_code" varchar(5) NOT NULL, 
+    "logo" varchar(255),
+    "rules" text,
+    "prizes" text, 
+    "details" text,
+    "schedule" text
+);
+
+
+
+CREATE TABLE "registrations" (
+  "id" SERIAL PRIMARY KEY,
+  "user_id" INT REFERENCES "user" NOT NULL,
+  "tournaments_id" INT REFERENCES "tournaments" NOT NULL
+);
+
+CREATE TABLE "games" (
+    "id" SERIAL PRIMARY KEY,
+    "games" text NOT NULL
+);
+
+
+
+CREATE TABLE "tournaments_games" (
+  "id" SERIAL PRIMARY KEY,
+  "games_id" INT REFERENCES "games" NOT NULL,
+  "tournaments_id" INT REFERENCES "tournaments_games" NOT NULL
+);
+
+CREATE TABLE "registration_games" (
+  "id" SERIAL PRIMARY KEY,
+  "registeration_id" INT REFERENCES "registrations" NOT NULL,
+  "tournament_game_id" INT REFERENCES "tournaments_games" NOT NULL
+);
+
+
+
+
+
