630d704ca8c1cd605f8cac1d74fc844fd191cb2e
Jonathan Palalay <ring5pun@hotmail.com>
2022-07-10 17:43:12 -0500

    added join button to details page

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect  } from 'react';
-
+import { useHistory } from 'react-router-dom';
 
 function Details(){
     const dispatch = useDispatch();
+    const history = useHistory();
+
     const params = useParams();
     console.log('params', params.id)
 
@@ -17,8 +19,24 @@ function Details(){
       });
   }, [params.id]);
 
-  const tournament = useSelector(store => store.details)
+  const tournament = useSelector(store => store.details);
+  const user = useSelector(store => store.user);
+
   console.log('in tournament', tournament);
+
+  const onLogin = (event) => {
+    history.push('/login');
+  };
+
+let timestamp = 1607110465663
+let date = new Date(timestamp);
+
+console.log(+date.getDate()+
+          "/"+(date.getMonth()+1)+
+          "/"+date.getFullYear()+
+          " "+date.getHours()+
+          ":"+date.getMinutes()+
+          ":"+date.getSeconds());
 
     return(
         <>
@@ -33,7 +51,18 @@ function Details(){
         <h4>{tournament && tournament.timezone}</h4>
         <h4>{tournament && tournament.isRegistrationOpen}</h4>
         <p>{tournament && tournament.rules}</p>
-        {/* <h4>{tournament && tournament.url }</h4> */}
+        <h4>{tournament && tournament.url }</h4>
+
+        {(user.id)?
+        <button className="btn btn_sizeSm" onClick={onLogin}>
+        Join Tournament
+        </button> 
+        :
+        <button className="btn btn_sizeSm" onClick={onLogin}>
+        Login
+        </button>
+
+      }
         </>
     )
   }
